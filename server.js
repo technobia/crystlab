3a67e569f84b9d2991f630c82378f6f12875bc2e
binh tran <binh.tran@8bitrockr.com>
2018-11-21 15:26:36 +0700

    initial theme

---
+++ server.js
@@ -0,0 +1,14 @@
+/**
+ * Created by binh on 9/10/18.
+ */
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/index.html');
+});
+
+app.listen(7000); //the port you want to use
+console.log("App running...");
